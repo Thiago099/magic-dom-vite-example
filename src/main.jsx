74cb11a125cb972e5649f16d62143ef4d37ca412
Thiago <thiago.kaique@stayfilm.com.br>
2023-06-29 13:44:48 -0300

    added state

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import "./style.css"
 
-var number = 0;
+var number = state(0);
 var main = 
 <div>
     <p>Magic Dom base project</p>
     <button on:click={add}>Counter: {number}</button>
 </div>
 
-
 main.$parent(document.body)
 
 function add() {
-    number ++;
-    main.$update();
+    number.$value ++;
 }
 
